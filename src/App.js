6a01a5c0e479a3b8d11493b85bb30c02e28c7d04
Mondal Bidisha <mondal.bidisha@talview.com>
2020-10-23 10:01:08 +0530

    removed unused files/images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import FormContainer from './Components/FormContainer';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact /> 
+        <Route path="/add-meeting" component={FormContainer} />
+      </Switch>
+    </main>
   );
 }
 
